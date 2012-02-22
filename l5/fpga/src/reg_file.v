f706f9d97e714d1aa8df5f6982f544d3c61a0475
CCS User <ccs@localhost.localdomain>
2012-02-22 12:58:30 -0600

    works

--- l5/fpga/src/reg_file.v
+++ l5/fpga/src/reg_file.v
@@ -40,31 +40,37 @@ module reg_file(addr,
       if (~rst) begin
          dout <= 32'hfee1dead;
          rw_cycle <= 1'b0;
-         RF[A_RAND] <= 32'h12345678;
+         RF[A_RAND] <= 32'h0;
       end
       
       else if (as) begin
-	 // Read
-	 if (~rs_n) begin
-            if (select == A_RAND) begin
-               dout <= RF[A_RAND];
-               RF[A_RAND] <= (RF[A_RAND] >> 1) ^ (-(RF[A_RAND] & 1) & 32'h80200003);
-            end else begin
-               dout <= 32'hbeefeade;
-            end // if (~rs_n)
-         end
-         
-	 // Write
-         else if (~ws_n) begin
-            if (select == A_RAND) begin
-               RF[A_RAND] <= din;
+	    // Read
+	    if (~rs_n && be[3]) begin
+               if (~rw_cycle) begin
+                  rw_cycle <= 1'b1;
+               
+                  if (select == A_RAND) begin
+                     dout <= RF[A_RAND];
+                     RF[A_RAND] <= (RF[A_RAND] >> 1) ^ (-(RF[A_RAND] & 1) & 32'h80200003);
+                  end else begin
+                     dout <= 32'hbeefeade;
+                  end // if (~rs_n)
+               end
             end
-	 end // if (as)
-         
-         // Reset
-         else begin
-            rw_cycle <= 1'b0;
-         end
+            
+	    // Write
+            else if (~ws_n && ~be[3]) begin
+               if (~rw_cycle) begin
+                  rw_cycle <= 1'b1;
+               
+                  if (select == A_RAND) begin
+                     RF[A_RAND] <= din;
+                  end
+               end
+            end
       end // if (as)
+      else begin
+         rw_cycle <= 0;
+      end // else: !if(as)
    end // always @ (posedge clk or negedge rst)
 endmodule // reg_file
