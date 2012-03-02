f47e6febc5628203b293645bd7a96245fa8369f2
Your Name <go2starr@gmail.com>
2012-03-02 14:03:24 -0600

    Restarting networking

--- l5/arm/FreeRTOS/board/tll6219/main.c
+++ l5/arm/FreeRTOS/board/tll6219/main.c
@@ -38,9 +38,12 @@
 #include "lwip/sys.h"
 #include "eth.h"
 
-#define PORT 2001
+#define PORT 2000
 #define NETBUF_SIZE 40
 #define SEED_RQST_BUF_SIZE 8
+#define BLK_SIZE 1
+#define SEED_CMD 1
+#define CHKSM_CMD 3
 #define REG_ADDR (*(volatile unsigned long*) 0xD3000000)
 /****************************************************************************
  *
@@ -52,8 +55,8 @@ void _main(void* parameter)
   struct netbuf *seed_rqst_buf;
   struct ip_addr addr;
 
-  int req_seed_msg[2];
-  int chksum_pkt_msg[10];
+  uint32_t chksum_pkt_msg[10];
+  uint32_t seed;
 
   err_t err;
   unsigned int count = 0; 
@@ -83,82 +86,88 @@ void _main(void* parameter)
 
   /* Bind to port */
   printf("Binding to port %d\n", PORT);
-  err = netconn_bind(conn, IP_ADDR_ANY, PORT);	
+  err = netconn_bind(conn, NULL, PORT);	
   if (err != ERR_OK) {
     printf("Failed to bind to port %d", PORT);
   }
 
+  /* Fill target IP address */
+  IP4_ADDR(&addr, 128, 104, 180, 237);
+
   /* Create a new net buffer */
-  printf("Creating a new netbuff\n");
+  printf("Creating netbufs for checksum packets and seed requests\n");
   buf = netbuf_new();
   seed_rqst_buf = netbuf_new();
- 
+
+//  netbuf_alloc(seed_rqst_buf, SEED_RQST_BUF_SIZE);
+//  netbuf_alloc(buf, BUF_SIZE);
 
   /* Reference buffer for sending checksum pkt */
-  printf("Creating reference to network buffer\n");
-  err = netbuf_ref(buf, (void*)chksum_pkt_msg, NETBUF_SIZE);
-  if (err != ERR_OK) {
-    printf("Error calling netbuf_ref for buf\n");
-    return;
-  } 
+//  printf("Creating reference to network buffer\n");
+//  err = netbuf_ref(buf, (void*)chksum_pkt_msg, NETBUF_SIZE);
+//  if (err != ERR_OK) {
+//    printf("Error calling netbuf_ref for buf\n");
+//   return;
+//  } 
+
+  // fill message
+  struct req_seed_pkt rsp;
+  rsp.command_val = htonl(SEED_CMD);
+  rsp.block_size = htonl(BLK_SIZE);
   
-  /* Reference buffer for requesting the seed */
+  //TODO stuff message in buffer
+//  seed_rqst_buf = (void*) req_seed_msg;
+
+  /* Reference buffer for requesiting the seed */
   printf("Creating reference to req_seed_msg buffer\n");
-  err = netbuf_ref(seed_rqst_buf, (void*)req_seed_msg, SEED_RQST_BUF_SIZE);
+  err = netbuf_ref(seed_rqst_buf, &rsp, SEED_RQST_BUF_SIZE);
   if (err != ERR_OK) {
     printf("Error calling netbuf_ref for seed_rqst_buf\n");
     return;
   }
-
-  // fill message
-  struct req_seed_pkt rsp;
-  rsp.command_val = 1;
-  rsp.block_size = 1;
-  req_seed_msg[0] = rsp.command_val;
-  req_seed_msg[1] = rsp.block_size;
-
-  // TODO create seed request packet
-  // All fields 32-bits and in network byte order
-  // Offset0 Command value = 1
-  // Offset4 Block size = 0-1023
-  //   Block size parameter 0; block size = 8KB
-  //   Block size parameter 1; block size = 16KB
-  //   Block size parameter 1023; block size = 8MB 
-  
-
+ 
   // convert between little endian and big endian (htonl & ntohl)
-
-  /* Fill target IP address */
-  IP4_ADDR(&addr, 128, 104, 180, 237);
-
-  // send packet to server
-  printf("Sending data to server\n");
+	// htonl host to network long
+	// ntohl network to host long
+  // send seed request packet to server
+  printf("Sending data to server on Port %d\n", PORT);
   err = netconn_sendto (conn, seed_rqst_buf, &addr, PORT);
   if (err != ERR_OK) {
     printf("Error %d: calling netconn_sendto\n", err);
     return;
   }
-    
-  // receive from server
+   
+  // receive seed from server
   printf("Waiting for data from server\n");
-  buf = netconn_recv (conn);
-  if (buf == NULL) {
-    printf("Error receiving data\n");
+  if ((seed_rqst_buf = netconn_recv(conn)) != NULL) {
+    void * data_ptr;
+    netbuf_copy(seed_rqst_buf, data_ptr, sizeof(seed_rqst_buf));
+    printf("Data received");
+
+  } else {
+    printf("Error receiving data: connection closed\n");
     return;
-  } 
-  printf("Data received");
-  //  netbuf_copy(buf, data_ptr, 13);
-  // printf("%s", (char*)data_ptr);
-  printf("Data received");
+  }
+
+  // TODO convert data received using ntohl, pass to SHA unit (or pass to unit, then convert?)
+
+//  void * data_ptr1;
+//  netbuf_copy(seed_rqst_buf, data_ptr, sizeof(seed_rqst_buf));
+//  printf ("%s", (char*) data_ptr)
+//  printf("\nData received");
   
 
   // TODO packet received from server -- convert to little endian
   // Offset0 Command value=1
   // Offset4 Seed value
-   
+  
+  // seed = ??;
+   netbuf_delete (seed_rqst_buf);
+ 
   // TODO checksum packets sent to server; each time one is sent count++
   // Offset0 Command value=3
-  // Offset4 Count ID (starts at 0 for first checksum pkt sent and increments for each subsequent packet
+  // Offset4 Count ID (starts at 0 for first
+  // checksum pkt sent and increments for each subsequent packet
   // Offset8 - Offset36 SHA256
 
   // u16_t netbuf_copy ( struct netbuf * aNetBuf, void * aData, u16_t aLen );  
